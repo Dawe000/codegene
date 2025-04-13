b2149186d383a2bc1e33bfab9ed8a86ab5e344ca
Dawe000 <dawid.pisarczyk@gmail.com>
2025-04-13 03:19:35 +0100

    attempt to fix weird deployment

--- website/src/index.tsx
+++ website/src/index.tsx
@@ -2,18 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+console.log('React script loading...');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+try {
+  const rootElement = document.getElementById('root');
+  console.log('Root element found:', rootElement);
+  
+  if (rootElement) {
+    const root = ReactDOM.createRoot(rootElement);
+    console.log('Root created, attempting to render...');
+    
+    root.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    );
+    console.log('Render called');
+  } else {
+    console.error('Root element not found');
+  }
+} catch (error) {
+  console.error('Error initializing React:', error);
+}
